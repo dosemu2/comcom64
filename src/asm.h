93222e0a6bddbd014d43e7b5b50317aba71156f3
Stas Sergeev <stsp2@yandex.ru>
2024-02-17 23:15:36 +0500

    glob_asmdefs.h is no longer needed

--- src/asm.h
+++ src/asm.h
@@ -2,11 +2,6 @@
 #define ASM_H
 
 #ifndef __ASSEMBLER__
-/* dummy out fdpp tricks for now */
-#define __ASMSYM(t) t*
-#define __ASMREF(t) &t
-#define __ASMFSYM(t) t**
-#define __ASYM(x) *(x)
 #include <dj64/glob_inc.h>
 
 #define ASMCFUNC
