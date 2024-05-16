7db62ba95739b1b049563ece6d38454d68b0d7af
Stas Sergeev <stsp@users.sourceforge.net>
2024-05-16 17:00:48 +0300

    build: add link.sh script
    This off-loads some things that meson can't do natively.

---
+++ src/link.sh
@@ -0,0 +1,18 @@
+LNK=$1
+shift
+OD=$1
+shift
+E=$1
+shift
+L=$1
+shift
+D=$1
+shift
+O=$1
+shift
+N=$1
+shift
+FLG=$($OD -T $L | grep _shm_flags | sed -E 's/([^ ]+) .+/0x\1/')
+CMD="$LNK -l $L -l $D -n $N -f $FLG -o $O $E"
+echo $CMD
+$CMD
