6d79b208a6f803ea98ee28353aeef63beb247056
Stas Sergeev <stsp@users.sourceforge.net>
2024-06-13 14:22:42 +0300

    link.sh: support llvm-objdump

--- src/link.sh
+++ src/link.sh
@@ -1,6 +1,9 @@
+die() {
+    echo "$1"
+    exit 1
+}
+
 LNK=$1
-shift
-OD=$1
 shift
 E=$1
 shift
@@ -12,6 +15,11 @@ O=$1
 shift
 N=$1
 shift
+
+OD=`which objdump 2>/dev/null`
+[ -n "$OD" ] || OD=`which llvm-objdump 2>/dev/null`
+[ -n "$OD" ] || die "objdump not found"
+
 FLG=$($OD -T $L | grep _shm_flags | sed -E 's/0{12}([^ ]+) .+/0x\1/')
 CMD="$LNK -d $D $L -n $N -f $FLG -o $O $* $E"
 echo $CMD
