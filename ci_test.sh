902deb03f49a5a96ae9f46d750e7300945085922
Andrew Bird <ajb@spheresystems.co.uk>
2025-01-17 10:39:36 +0000

    CI: output boot log on failure

--- ci_test.sh
+++ ci_test.sh
@@ -6,7 +6,14 @@ sudo dpkg -i ../comcom64*.deb
 
 . ./ci_test_prereq.sh
 
-dosemu -td -E ver
+if ! dosemu -td -o boot.log -E ver ; then
+  {
+    echo "================== boot.log ==================="
+    cat boot.log
+    echo "==============================================="
+  } >&2
+  exit 1
+fi
 
 make 32 -j 9
 # make sure 32bit version also built
