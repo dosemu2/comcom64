a143d291d7f5612615b2817b96be0b360d104dbe
Stas Sergeev <stsp@users.sourceforge.net>
2025-02-03 00:42:21 +0300

    switch to djlink 0.6

--- src/link.sh
+++ src/link.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 die() {
     echo "$1"
     exit 1
@@ -11,14 +13,12 @@ D=$1
 shift
 O=$1
 shift
-N=$1
-shift
 
 OD=`which objdump 2>/dev/null`
 [ -n "$OD" ] || OD=`which llvm-objdump 2>/dev/null`
 [ -n "$OD" ] || die "objdump not found"
 
 FLG=$($OD -T $L | grep _shm_flags | sed -E 's/0{12}([^ ]+) .+/0x\1/')
-CMD="$LNK -d $D $L -n $N -f $FLG -o $O $*"
+CMD="$LNK -d $D $L -f $FLG -o $O $*"
 echo $CMD
 $CMD
