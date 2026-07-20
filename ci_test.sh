6807dffbd6aca0542428b1241e438ba626be24b4
Andrew Bird <ajb@spheresystems.co.uk>
2026-07-20 20:06:58 +0100

    CI: Use the test scripts from Dosemu
    Test comcom32/64 using the suite from dosemu2

--- ci_test.sh
+++ ci_test.sh
@@ -2,14 +2,30 @@
 
 set -e
 
-if ! dosemu -td -o boot.log -E ver ; then
-  {
-    echo "================== boot.log ==================="
-    cat boot.log
-    echo "==============================================="
-  } >&2
-  exit 1
-fi
+export TEST_DOSEMU=/usr/bin/dosemu
+export TEST_CMDDIR=/usr/share/dosemu/dosemu2-cmds-0.3
+export NO_FAILFAST=1
+
+cat >&2 << EOF3
+=====================================================
+=              Tests run on Comcom32                =
+=====================================================
+EOF3
+env COPY_COMMAND_COM=/usr/share/comcom32/comcom32.exe test/test_comcom.py TestCase32
+
+cat >&2 << EOF4
+=====================================================
+=              Tests run on Comcom64                =
+=====================================================
+EOF4
+env COPY_COMMAND_COM=/usr/share/comcom64/comcom64.exe test/test_comcom.py TestCase64
 
 make both -j 9
 ls -l src/32/comcom32.exe
+
+# Return non-zero if any logfiles were generated
+for i in test_*.*.*.log ; do
+  test -f $i || exit 0
+done
+
+exit 1
