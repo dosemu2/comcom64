d440ccd92b855da38eb75e97d95f88e080dc4b1a
Andrew Bird <ajb@spheresystems.co.uk>
2026-09-17 10:00:44 +0100

    CI: Populate Cache needs the test scripts

--- ci_prereq.sh
+++ ci_prereq.sh
@@ -9,3 +9,10 @@ sudo apt install -y \
   equivs
 
 mk-build-deps --install --root-cmd sudo
+
+# Ensure the test-binaries link points to ~/cache
+[ -h "test-binaries" ] || ln -s "${HOME}"/cache "test-binaries"
+
+# Now grab parts of dosemu2 that we need
+wget --no-verbose https://github.com/dosemu2/dosemu2/archive/refs/heads/devel.tar.gz
+tar -xf devel.tar.gz --strip-components=1 dosemu2-devel/test
--- ci_test_prereq.sh
+++ ci_test_prereq.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-# Ensure the test-binaries link points to ~/cache
-[ -h "test-binaries" ] || ln -s "${HOME}"/cache "test-binaries"
-
-# Now grab parts of dosemu2 that we need
-wget --no-verbose https://github.com/dosemu2/dosemu2/archive/refs/heads/devel.tar.gz
-tar -xf devel.tar.gz --strip-components=1 dosemu2-devel/test
-
 sudo add-apt-repository -y --no-update ppa:dosemu2/ppa
 sudo add-apt-repository -y --no-update ppa:jwt27/djgpp-toolchain
 sudo add-apt-repository -y --no-update ppa:stsp-0/gcc-ia16
